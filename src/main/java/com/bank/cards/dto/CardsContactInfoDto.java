7d1dd652edfd7ffc7994a9cc97d55687d6ed7868
saurabh <saurabh.kumar.2382000@gmail.com>
2024-09-17 22:36:54 +0530

    replaced record with POJO class for the config

--- src/main/java/com/bank/cards/dto/CardsContactInfoDto.java
+++ src/main/java/com/bank/cards/dto/CardsContactInfoDto.java
@@ -8,6 +8,10 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "cards")
-public record CardsContactInfoDto(String message, Map<String, String> contactDetails,List<String> onCallSupport)
-{
+@Getter
+@Setter
+public class CardsContactInfoDto{
+	private String message;
+	private Map<String, String> contactDetails;
+	private List<String> onCallSupport;
 }
